e15db012f515519016857c92aed36688d1d03c38
MD Naimur Rahman <naimurtsc567@gmail.com>
2023-05-30 11:08:51 +0600

    New Branch created

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application, Request, Response } from "express"
-import cors from "cors"
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 
 // Configuring app
 const app: Application = express()
@@ -10,10 +10,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
-    res.status(201).send({
-        message: "Auth Service Management Server is running successfully.",
-        statusCode: 201
-    })
+  res.status(201).send({
+    message: 'Auth Service Management Server is running successfully.',
+    statusCode: 201,
+  })
 })
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function main() {
-    try {
-        await mongoose.connect(`${config.database_url}`);
-        console.log(`🛢 Database Connected`);
-        app.listen(config.port, () => {
-            console.log(`Server is running on http://localhost:${config.port}`);
-        })
-    }
-    finally {
-
-    }
-
+  try {
+    await mongoose.connect(`${config.database_url}`)
+    console.log(`🛢 Database Connected`)
+    app.listen(config.port, () => {
+      console.log(`Server is running on http://localhost:${config.port}`)
+    })
+  } finally {
+    /* empty */
+  }
 }
 
-main().catch(err => console.log(err));
+main().catch(err => console.log(err))
