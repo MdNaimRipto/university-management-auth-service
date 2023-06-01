0b2a9fa0ea67cf702c063e35c3fa354eb05dbef3
MD Naimur Rahman <naimurtsc567@gmail.com>
2023-06-01 10:34:30 +0600

    User Interface, Schema and Model created

---
+++ src/app/modules/users/users.schema.ts
@@ -0,0 +1,18 @@
+import { Model, Schema, model } from 'mongoose'
+import { IUsers } from './users.interface'
+
+type UserModel = Model<IUsers, object>
+
+const userSchema = new Schema<IUsers>(
+  {
+    id: { type: String, required: true, unique: true },
+    role: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export const Users = model<IUsers, UserModel>('Users', userSchema)
